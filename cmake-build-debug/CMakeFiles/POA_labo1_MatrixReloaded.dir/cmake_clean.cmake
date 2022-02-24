b35fb3be16dc11d6856f01c6651b8759e9e8ba5f
chloefont <cfontaine@citycable.ch>
2022-02-24 15:33:34 +0100

    base

---
+++ cmake-build-debug/CMakeFiles/POA_labo1_MatrixReloaded.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POA_labo1_MatrixReloaded.dir/main.cpp.obj"
+  "POA_labo1_MatrixReloaded.exe"
+  "POA_labo1_MatrixReloaded.exe.manifest"
+  "POA_labo1_MatrixReloaded.pdb"
+  "libPOA_labo1_MatrixReloaded.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POA_labo1_MatrixReloaded.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/POA_labo1_MatrixReloaded.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for POA_labo1_MatrixReloaded.
+# This may be replaced when dependencies are built.
